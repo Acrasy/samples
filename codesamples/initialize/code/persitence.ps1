6b11f1997d0ff02efde925f98e6f39205b469aac
Rommer Simon <rommer.s@ik.local>
2019-10-16 15:28:42 +0200

    remove cmt

--- codesamples/initialize/code/persitence.ps1
+++ codesamples/initialize/code/persitence.ps1
@@ -1,14 +1,3 @@
-#Bypass execution policy via python exec of powershell
-#Key to change run permission of scripts on local machine
-#HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.Powershell
-#PS-cmd to read Key
-#Get-ItemProperty -Path HKLM:\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.Powershell
-
-#set regkey entry
-#$keyName = "ExecutionPolicy"
-#$keyValue = "RemoteSigned"
-#Set-ItemProperty -Path 'HKLM:\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.Powershell' -Name $keyName -Value $keyValue
-
 $whoami=whoami.exe
 Set-ExecutionPolicy Bypass -Scope Process -Force;
 
